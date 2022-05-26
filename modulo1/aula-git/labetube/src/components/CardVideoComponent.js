2a4d6e1b8cc4645ab7fc97a9e4d526b13a898eec
Naiara Santos Gomes <naiaragms2018@gmail.com>
2022-05-26 13:27:38 -0300

    adicionei-projeto-labetin

---
+++ modulo1/aula-git/labetube/src/components/CardVideoComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CardVideoComponent () { 
+return (
+         <div className="card-video midia">
+              <img src="https://picsum.photos/400/400?a=1 " alt="Imagem do vídeo" />
+               className='card-controls'>><h2>Titulo do primeiro video</h2> <button>Play</button>
+</div>
+</div>
+)
+}
+
+export { CardVideoComponent }
---
+++ modulo1/labedin/src/components/CardPequeno/CardPequeno.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CardPequeno.css'
+
+function CardPequeno (props) {
+return (
+    <div className="small-container">
+    <img src={ props.imagem }/>
+    <p>{ props.texto }</p>
+</div>
+
+)
+}
+
+export default CardPequeno;
