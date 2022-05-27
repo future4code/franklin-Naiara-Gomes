b69790e0f94bd1e21225d2d5a8a636af893995bf
Naiara Santos Gomes <naiaragms2018@gmail.com>
2022-05-27 00:06:29 -0300

    modificacoes-styled-components-ok

--- modulo1/labedin/src/components/CardPequeno/CardPequeno.js
+++ modulo1/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import './CardPequeno.css'
+import styled from 'styled-components';
 
+const CardPequenoAn = styled.div`
+   display: flex;
+    align-items: center;
+    border: 1px solid blue;
+    border-none: 50px;
+    width: 540px;
+    padding: 15px 30px;
+    margin: 10px auto;
+`
+const CardPequenoImg = styled.img`
+    width: 30px;
+    margin-right: 10px;
+`
 function CardPequeno (props) {
 return (
-    <div className="small-container">
-    <img src={ props.imagem }/>
+    <CardPequenoAn>
+    <CardPequenoImg src={ props.imagem }/>
     <p>{ props.texto }</p>
-</div>
+</CardPequenoAn>
 
 )
 }
