1c0545b91684c5463136920095ca52468c62b312
Naiara Santos Gomes <naiaragms2018@gmail.com>
2022-06-03 15:27:02 -0300

    chat-lateral

---
+++ modulo1/projeto-whatslab/projeto-whatslab/modulo1/aula-git/whatslab/projeto-whatslab/src/components/ChatListItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ChatListItem.css';
+
+const Chat = ( ) => {
+    return (
+        <div className="chatListItem">
+        <img className="chatListItem--avatar" src="https://cdn-icons-png.flaticon.com/512/194/194938.png" alt="" />
+        <div className="chatListItem--lines">
+        <div className="chatListItem--line">
+
+        <div className="chatListItem--name">Francine </div>
+        <div className="chatListItem--date">19:00 </div>
+              </div>
+              <div className="chatListItem--line">
+              <div className="chatListItem--lastMsg">
+                  <p>"Opa, tudo bem?'</p>
+              </div>
+              </div>
+           </div>
+        </div>
+    );
+}
+export default Chat;
+
