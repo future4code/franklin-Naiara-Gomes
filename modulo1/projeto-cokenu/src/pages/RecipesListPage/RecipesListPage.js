9c211b1c8beec5f3458f66ed9886bed6748ce9d5
Naiara Santos Gomes <naiara.gomes@grupoboticario.com.br>
2022-07-04 13:59:52 -0300

    implementado botao de adicionar receitas

--- modulo1/projeto-cokenu/src/pages/RecipesListPage/RecipesListPage.js
+++ modulo1/projeto-cokenu/src/pages/RecipesListPage/RecipesListPage.js
@@ -4,18 +4,26 @@ import { BASE_URL } from "../../constants/urls";
 //import RecipeCard from "../../components/RecipeCard/RecipeCard"
 import useProtectedPage from "../../hooks/useProtectedPage";
 import useRequestData from "../../hooks/useRequestData";
+import { RecipeListContainer, AddRecipeButton } from "./styled";
+import {Add} from '@material-ui/icons'
+import { goToAddRecipes } from "../../routes/coordinator";
+import { useNavigate } from "react-router-dom";
+//import { goToAddRecipes } from "../../routes/coordinator";
 
 const RecipesListPage = () => {
   useProtectedPage()
+  const navigate = useNavigate()
   const recipes = useRequestData([],`${BASE_URL}/recipe/feed`)
-  console.log(recipes)
+  
 
  const recipeCards = recipes.map((recipe)=>{
+   //return (<p>{recipe.title}</p>)
   
-  return (
+   return (
     
     <RecipeCard 
-        title={"title"}
+        key={recipe.recipe_id}
+        title={"recipe.title"}
         image={recipe.image}
         onClick={()=> null}
       />
@@ -25,11 +33,15 @@ const RecipesListPage = () => {
 
 
   return (
-    <div>
-     
-     
+    <RecipeListContainer> 
        {recipeCards}
-    </div>
+       <AddRecipeButton
+       color={"primary"}
+       onClick={()=>goToAddRecipes(navigate)}
+        >
+          <Add/>
+       </AddRecipeButton> 
+       </RecipeListContainer>
   );
 };
 
